7f57ceca75c6e51dab0b6897de26dc263307eb99
ItaDaur <96618793+ItaDaur@users.noreply.github.com>
2022-04-19 10:41:21 +0600

    first commit

---
+++ Otrar/Modul.swift
@@ -0,0 +1,9 @@
+//
+//  Modul.swift
+//  Otrar
+//
+//  Created by Dauren Sarsenov on 19.04.2022.
+//
+
+import Foundation
+
---
+++ Otrar/ProfileViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileViewController.swift
+//  Otrar
+//
+//  Created by Dauren Sarsenov on 19.04.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var infoBooking: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+    }
+    
+    
+    
+    
+    
+    
+}
+
+
+
